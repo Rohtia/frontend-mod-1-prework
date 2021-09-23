b94e9d41facfb4b6b1e6b2e8e29c673b0e12f6c7
Rohtia <90939780+Rohtia@users.noreply.github.com>
2021-09-22 22:32:58 -0400

    Section 3 midpoint

---
+++ section3/arrayMethods.md
@@ -0,0 +1,18 @@
+`pop`
+  Like popping a balloon
+  this method pops the last element (the last item on the list) off of the array and returns it
+
+`push`
+  Pushes the length of the array along a number line
+  This adds the item (or items) to the end of the array and returns the new length of the array.
+
+`shift`
+  Like pop, but at the front of the array.
+  removes the first element of the array and returns it.
+
+`unshift`
+  The opposite of `shift`
+  This adds one (or more) elements to the start of the array and returns the new length of the array.
+
+
+**Of these four, the methods that *remove* an element return the value of that element. The methods that *add* an element return the new length of the array**
--- section3/exercises/arrays.js
+++ section3/exercises/arrays.js
@@ -24,42 +24,51 @@ console.log(animals[0]);
 
 // YOU DO: Write code below that will log the number of elements in array of
 // animals from above.
-
+console.log(animals.length);
 
 // YOU DO: Write code that will reassign the last item in the animals
 // array to "Gorilla"
-
+animals[2] = 'Gorilla';
 
 // YOU DO: Write code that will add a new animal (type of your choice) to position 3.
-
+animals.push('Shark');
+// console.log(animals[3]);
 
 // YOU DO: Write code that will log the String "Elephant" in the animals array
-
+var num = animals.push('Elephant');
+console.log(animals[num-1]);
 
 //-------------------
 // PART 2: Foods: Array Methods
 //-------------------
 
 // YOU DO: Declare a variable that will store an an array of at least 4 foods (strings)
-
+var foods = ['apple', 'orange', 'cheese', 'bread', 'pasta'];
 
 // YOU DO: Write code below that will log the number of elements in the array of
 // foods from above.
-
+console.log(foods.length);
 
 // YOU DO: Write code below that uses a method to add "broccoli" to the foods array and
 // log the changed array to verify "broccoli" has been added
-
+foods.push('broccoli');
+console.log(foods);
 
 // YOU DO: Write code below that removes the last item of food from the foods array and
 // log the changed array to verify that item has been removed
+foods.pop();
+console.log(foods);
 
-
-// YOU DO: Write code to add 3 new foods to the array. 
+// YOU DO: Write code to add 3 new foods to the array.
   // There are several ways to do this - choose whichever you'd like!
 // Then, log the changed array to verify the new items have been added
+foods.unshift('grapes', 'bagel', 'meatball');
+console.log(foods);
 
 // YOU DO: Remove the food that is in index position 0.
+foods.shift();
+//console.log(foods);
+
 
 //-------------------
 // PART 3: Where are Arrays used?
@@ -79,12 +88,12 @@ The post itself likely has more complex data, but here's one way we can think ab
 var posts = ["image at beach", "holiday party", "adorable puppy", "video of cute baby"];
 
 // YOU DO: Think of a web application you commonly use. Where do you see LISTS utilized, where arrays
-// may be storing data? Come up with 3 examples - they could be from different web applications or 
+// may be storing data? Come up with 3 examples - they could be from different web applications or
 // all from the same one.
 
-// 1:
-// 2:
-// 3:
+// 1: People you might know
+// 2: comments on tweets
+// 3: followers list
 
 
 //-------------------
@@ -96,7 +105,7 @@ YOU DO:
 Using the variables defined below, write a program that will tell a user if they
 will be able to call an Uber.
 
-The user can call an uber if they have 15% battery remaining, or more. In this case, it doesn't matter 
+The user can call an uber if they have 15% battery remaining, or more. In this case, it doesn't matter
 if the user has a charger at all, or what type.
 The can call an uber if they have a charger and it is a car charger.
 
@@ -106,7 +115,28 @@ var hasCharger = true;
 var chargerType = "car";
 
 // Write your conditional here
-
+console.log('Can I call an uber?');
+// Conditional to check if the battery is charged enough
+if (percentBatteryLeft >= 15) {
+  console.log('Yes, I can call an uber.');
+// If the above was false, then they cannot call an uber
+} else {
+  console.log('No, I cannot call an uber.');
+  console.log('Do I have a charger?');
+  // Check strict equivilancy to see if they have a charger; they may still be able to call
+  if (hasCharger === true) {
+    // They can only call with the right kind of charger. equivilancy check for the kind of charger.
+    if (chargerType == 'car') {
+      console.log('I have a charger. Yes I can call an uber.');
+    // If the above was false (wrong kind of charger), they cannot.
+    } else {
+      console.log("I have a charger, but I still can't call an uber.");
+    }
+  // A false returned from the corresponding if statement.
+  } else {
+    console.log('I cannot call an uber.');
+  }
+}
 
 // YOU DO - ANNOTATE: Above each line of your code for this if statement, create a comment.
 // That comment should describe, in your own words, and as technically precise as possible,
